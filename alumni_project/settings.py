b48787f7d21d7c289503a56c1484c99896619564
rahulraj2412003 <115562691+rahulraj2412003@users.noreply.github.com>
2026-01-01 19:30:46 +0530

    Update settings.py

--- alumni_project/settings.py
+++ alumni_project/settings.py
@@ -162,6 +162,4 @@ CLOUDINARY_STORAGE = {
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
-
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
