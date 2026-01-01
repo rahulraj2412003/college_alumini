7055e8c8ede4dd3e15b203452cad63925b372184
rahulraj2412003 <115562691+rahulraj2412003@users.noreply.github.com>
2026-01-01 19:54:08 +0530

    Update settings.py

--- alumni_project/settings.py
+++ alumni_project/settings.py
@@ -162,4 +162,12 @@ CLOUDINARY_STORAGE = {
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# Use this instead of DEFAULT_FILE_STORAGE
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
